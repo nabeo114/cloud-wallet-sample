ccaa565f8b51a5c510eb7b28fc0974904e28075c
nabeo114 <49563792+nabeo114@users.noreply.github.com>
2024-08-17 16:44:30 +0900

    NFT画像にトークンID毎のウォーターマークを入れるように変更

--- backend/scripts/assetUtils.js
+++ backend/scripts/assetUtils.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 
 // 動的にNFTメタデータを生成する非同期関数
 async function generateNFTMetadata(tokenId) {
@@ -14,8 +15,7 @@ async function generateNFTMetadata(tokenId) {
     }
 
     // 画像ファイルのパスを設定（assetsディレクトリ内のファイル）
-//    const imageFilePath = path.join(__dirname, '..', 'assets', `${tokenId}.png`);
-    const imageFilePath = path.join(__dirname, '..', 'assets', '0.png');
+    const imageFilePath = path.join(__dirname, '..', 'assets', 'image.png');
 
     // 画像ファイルが存在しない場合の処理
     if (!fs.existsSync(imageFilePath)) {
@@ -27,8 +27,7 @@ async function generateNFTMetadata(tokenId) {
     const metadata = {
       name: `Sample NFT #${tokenId}`,
       description: "This is a sample NFT for demonstration purposes. It showcases the basic structure of an NFT's metadata, including a sample image and descriptive text.",
-//      image: `http://localhost:5000/assets/image/${tokenId}.png`,
-      image: 'http://localhost:5000/assets/image/0.png',
+      image: `http://localhost:5000/assets/image/${tokenId}.png`,
     };
 
     return metadata;
@@ -41,7 +40,7 @@ async function generateNFTMetadata(tokenId) {
 async function generateNFTImage(imageFile) {
   try {
     // 画像ファイルのパスを設定（assetsディレクトリ内のファイル）
-    const imageFilePath = path.join(__dirname, '..', 'assets', `${imageFile}`);
+    const imageFilePath = path.join(__dirname, '..', 'assets', 'image.png');
 
     // 画像ファイルが存在しない場合の処理
     if (!fs.existsSync(imageFilePath)) {
@@ -50,11 +49,33 @@ async function generateNFTImage(imageFile) {
     }
 
     // 画像ファイルを読み込み
-    const imageData = await fs.promises.readFile(imageFilePath)
+    const image = sharp(imageFilePath);
+    const { width, height } = await image.metadata();
 
-    return imageData;
+    // ウォーターマーク用のSVGテキストを作成
+    const svgText = `
+      <svg width="${width}" height="${height}">
+        <text x="50%" y="50%"
+              font-size="128"
+              font-family="Arial"
+              fill="white"
+              fill-opacity="0.2"
+              text-anchor="middle"
+              alignment-baseline="middle">${imageFile}</text>
+      </svg>`;
+
+    // SVGテキストをバッファに変換
+    const watermark = Buffer.from(svgText);
+
+    // 画像にウォーターマークを合成
+    const watermarkedImage = await image
+      .composite([{ input: watermark, gravity: 'center' }])  // ウォーターマークを中央に配置
+      .png()  // PNG形式で画像を出力
+      .toBuffer();  // 画像をバッファに変換して返す
+
+    return watermarkedImage;
   } catch (error) {
-    throw new Error(`Failed to generate NFT metadata: ${error.message}`);
+    throw new Error(`Failed to generate NFT image: ${error.message}`);
   }
 }
 
